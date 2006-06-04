c27c873830a73dcf2cb75925c0f09c966def3ac0
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2006-06-04 07:20:44 +0000

    Added packaging infrastructure. Cleaned up configs.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@46 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/jam-config.rb
+++ config/jam-config.rb
@@ -1,14 +1,14 @@
 
 require 'etc'
+require 'tsc/platform.rb'
 
 class JamConfig
   def options
     [ 
-      '-s', "JAMCONFIG_PRODUCT_NAME=stem-cpp", 
-      '-s', "JAMCONFIG_PRODUCT_RELEASE=1.0", 
+      '-s', "JAMCONFIG_PRODUCT_NAME=stemkit-cpp", 
+      '-s', "JAMCONFIG_PRODUCT_RELEASE=0.0.1", 
       '-s', "JAMCONFIG_PRODUCT_BUILD=0", 
-      '-s', "JAMCONFIG_PRODUCT_PLATFORM=macos-ppc",
-
+      '-s', "JAMCONFIG_PRODUCT_PLATFORM=#{TSC::Platform.current.name}",
       '-s', "JAMCONFIG_LIBRARY_PREFIX=", 
       '-s', "JAMCONFIG_LIBRARY_MAJOR=1", 
 
