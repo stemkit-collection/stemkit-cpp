1e795cd68bc89158d82ea0cc62e66c1c477513e3
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2006-06-16 14:18:08 +0000

    Added tests for file descriptor streams.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@82 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/io/test/FileDescriptorStreamsTest.cc
@@ -0,0 +1,90 @@
+/*  Copyright (c) 2006, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+*/
+
+#include "FileDescriptorStreamsTest.h"
+#include <sk/util/SystemException.h>
+#include <sk/io/FileDescriptorInputStream.h>
+#include <sk/io/FileDescriptorOutputStream.h>
+#include <sk/io/DataInputStream.h>
+#include <sk/io/DataOutputStream.h>
+#include <sk/io/EOFException.h>
+
+CPPUNIT_TEST_SUITE_REGISTRATION(sk::io::test::FileDescriptorStreamsTest);
+
+sk::io::test::FileDescriptorStreamsTest::
+FileDescriptorStreamsTest()
+{
+}
+
+sk::io::test::FileDescriptorStreamsTest::
+~FileDescriptorStreamsTest()
+{
+}
+
+void
+sk::io::test::FileDescriptorStreamsTest::
+setUp()
+{
+  int fds[2];
+  if(pipe(fds) != 0) {
+    throw sk::util::SystemException("pipe() failed");
+  }
+  _inputStreamHolder.set(new sk::io::FileDescriptorInputStream(fds[0]));
+  _outputStreamHolder.set(new sk::io::FileDescriptorOutputStream(fds[1]));
+  _dataInputStreamHolder.set(new sk::io::DataInputStream(_inputStreamHolder.get()));
+  _dataOutputStreamHolder.set(new sk::io::DataOutputStream(_outputStreamHolder.get()));
+}
+
+void
+sk::io::test::FileDescriptorStreamsTest::
+tearDown()
+{
+  _dataInputStreamHolder.clear();
+  _dataOutputStreamHolder.clear();
+
+  _inputStreamHolder.clear();
+  _outputStreamHolder.clear();
+
+}
+
+void
+sk::io::test::FileDescriptorStreamsTest::
+testReadWrite()
+{
+  dataOutput().writeInt(1234);
+  CPPUNIT_ASSERT_EQUAL(1234, dataInput().readInt());
+
+  dataOutput().writeChars("Hello\n");
+  CPPUNIT_ASSERT_EQUAL(sk::util::String("\"Hello\\n\""), dataInput().readLine().inspect());
+}
+
+void
+sk::io::test::FileDescriptorStreamsTest::
+testClose()
+{
+  output().close();
+  CPPUNIT_ASSERT_THROW(dataInput().readChar(), sk::io::EOFException);
+}
+
+void
+sk::io::test::FileDescriptorStreamsTest::
+testCloseOnDestruction()
+{
+  _outputStreamHolder.clear();
+  CPPUNIT_ASSERT_THROW(dataInput().readChar(), sk::io::EOFException);
+}
+
+void
+sk::io::test::FileDescriptorStreamsTest::
+testPartialReadOnEof()
+{
+  dataOutput().writeChars("Hello, it's me");
+  output().close();
+
+  CPPUNIT_ASSERT_EQUAL(sk::util::String("Hello, it's me"), dataInput().readLine());
+  CPPUNIT_ASSERT_THROW(dataInput().readChar(), sk::io::EOFException);
+}
---
+++ lib/sk/io/test/FileDescriptorStreamsTest.h
@@ -0,0 +1,70 @@
+/*  Copyright (c) 2006, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+*/
+
+#ifndef _SK_IO_TEST_FILEDESCRIPTORSTREAMSTEST_
+#define _SK_IO_TEST_FILEDESCRIPTORSTREAMSTEST_
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <sk/util/Holder.h>
+
+namespace sk {
+  namespace io {
+    class InputStream;
+    class OutputStream;
+    class DataInput;
+    class DataOutput;
+
+    namespace test {
+      class FileDescriptorStreamsTest
+        : public CppUnit::TestFixture
+      {
+        CPPUNIT_TEST_SUITE(sk::io::test::FileDescriptorStreamsTest);
+          CPPUNIT_TEST(testReadWrite);
+          CPPUNIT_TEST(testClose);
+          CPPUNIT_TEST(testCloseOnDestruction);
+          CPPUNIT_TEST(testPartialReadOnEof);
+        CPPUNIT_TEST_SUITE_END();
+        
+        public:
+          FileDescriptorStreamsTest();
+          virtual ~FileDescriptorStreamsTest();
+          
+          void setUp();
+          void tearDown();
+          void testReadWrite();
+          void testClose();
+          void testCloseOnDestruction();
+          void testPartialReadOnEof();
+          
+        private:
+          FileDescriptorStreamsTest(const FileDescriptorStreamsTest& other);
+          FileDescriptorStreamsTest& operator = (const FileDescriptorStreamsTest& other);
+
+          sk::io::InputStream& input() {
+            return _inputStreamHolder.get();
+          }
+          sk::io::OutputStream& output() {
+            return _outputStreamHolder.get();
+          }
+          sk::io::DataInput& dataInput() {
+            return _dataInputStreamHolder.get();
+          }
+          sk::io::DataOutput& dataOutput() {
+            return _dataOutputStreamHolder.get();
+          }
+
+          sk::util::Holder<sk::io::InputStream> _inputStreamHolder;
+          sk::util::Holder<sk::io::OutputStream> _outputStreamHolder;
+          sk::util::Holder<sk::io::DataInput> _dataInputStreamHolder;
+          sk::util::Holder<sk::io::DataOutput> _dataOutputStreamHolder;
+      };
+    }
+  }
+}
+
+#endif /* _SK_IO_TEST_FILEDESCRIPTORSTREAMSTEST_ */
