1d728ec9b4386e65ab5c1a06b54fa5ccaedb2aa3
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2006-06-18 21:28:12 +0000

    Adding an experimental class to be used in better implementation of file stream classes. Requires refinements for sk::io::File class as well.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@85 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ include/sk/io/FileStreamCoupler.h
@@ -0,0 +1,50 @@
+/*  Copyright (c) 2006, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+*/
+
+#ifndef _SK_IO_FILESTREAMCOUPLER_
+#define _SK_IO_FILESTREAMCOUPLER_
+
+#include <sk/util/Holder.h>
+#include <sk/io/File.h>
+#include <memory>
+
+namespace sk {
+  namespace io {
+    template<class Stream>
+    class FileStreamCoupler
+      : public Stream
+    {
+      public:
+        FileStreamCoupler(std::auto_ptr<sk::io::File> file_auto_ptr);
+
+        sk::io::File& getFile() const;
+        
+      private:
+        FileStreamCoupler(const FileStreamCoupler& other);
+        FileStreamCoupler& operator = (const FileStreamCoupler& other);
+
+        sk::util::Holder<sk::io::File> _fileHolder;
+    };
+  }
+}
+
+template<class Stream>
+sk::io::FileStreamCoupler<Stream>::
+FileStreamCoupler(std::auto_ptr<sk::io::File> file_auto_ptr)
+  : Stream(file_auto_ptr.get()->getFileDescriptor()), _fileHolder(file_auto_ptr.release())
+{
+}
+
+template<class Stream>
+sk::io::File&
+sk::io::FileStreamCoupler<Stream>::
+getFile() const
+{
+  return _fileHolder.get();
+}
+
+#endif /* _SK_IO_FILESTREAMCOUPLER_ */
