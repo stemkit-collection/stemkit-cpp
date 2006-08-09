6edb1669113cb832e6e959f57b8ab5aae6e1f775
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2006-08-09 19:01:31 +0000

    Added utility 'stemkit-info'.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@131 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ install/make-info.rb
@@ -0,0 +1,19 @@
+# Copyright (c) 2006, Gennady Bystritsky <bystr@mac.com>
+# 
+# Distributed under the MIT Licence.
+# This is free software. See 'LICENSE' for details.
+# You must read and accept the license prior to use.
+
+if defined? Installation::Generator
+  class Generator < Installation::Generator
+    def create(io)
+      bindir = File.join(self.class.installation_tools, 'bin')
+      [
+        '#!/bin/sh',
+        'exec ' + File.join(self.class.installation_tools, 'bin', 'tpm-info') + ' "${@}"'
+      ]
+    end
+  end
+
+  throw :generator, Generator
+end
