6e1bc99b24c4d57a39dd3ffd65ef425f6e3f35c8
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-02-05 05:01:12 +0000

    Added platform specific unistd.h wrapper for Mac OX Tiger to compensate for non-standard setpgrp() call. Rearranged setup of platform specific include path for all platforms.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@518 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ platform/include/macos-tiger/unistd.h
@@ -0,0 +1,15 @@
+/*  Copyright (c) 2008, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+*/
+
+#ifndef _PLATFORM_MACOS_TIGER_UNISTD_
+#define _PLATFORM_MACOS_TIGER_UNISTD_
+
+#include <../include/unistd.h>
+
+#define setpgrp() setpgid(getpid(), getpid())
+
+#endif /* _PLATFORM_MACOS_TIGER_UNISTD_ */
