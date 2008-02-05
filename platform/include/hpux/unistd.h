d36c992e0a0bd0c2de5d069cdd87235473346325
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-02-05 06:41:35 +0000

    Added platform specific unistd.h for hpux to compensate for the lack fo seteuid() function.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@521 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ platform/include/hpux/unistd.h
@@ -0,0 +1,15 @@
+/*  Copyright (c) 2008, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+*/
+
+#ifndef _PLATFORM_HPUX_UNISTD_
+#define _PLATFORM_HPUX_UNISTD_
+
+#include <../include/unistd.h>
+
+#define seteuid(euid) setreuid(-1, euid)
+
+#endif /* _PLATFORM_HPUX_UNISTD_ */
