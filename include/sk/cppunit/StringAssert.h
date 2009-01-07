339f3ddaf3b38f358f148f7566361ef6a915acfc
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-01-07 16:38:42 +0000

    save
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@884 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- include/sk/cppunit/StringAssert.h
+++ include/sk/cppunit/StringAssert.h
@@ -16,60 +16,31 @@
 CPPUNIT_NS_BEGIN
 
 template<>
-struct assertion_traits<std::string>
-{
-    static bool equal( const std::string& x, const std::string& y )
-    {
+struct assertion_traits<std::string> {
+    static bool equal(const std::string& x, const std::string& y) {
         return sk::util::String(x).equals(y);
     }
 
-    static std::string toString( const std::string& x )
-    {
+    static std::string toString(const std::string& x) {
         return sk::util::String(x).inspect();
     }
 };
 
 template<>
-struct assertion_traits<sk::util::String>
-{
-    static bool equal(const sk::util::String& x, const sk::util::String& y)
-    {
+struct assertion_traits<sk::util::String> {
+    static bool equal(const sk::util::String& x, const sk::util::String& y) {
         return x.equals(y);
     }
 
-    static std::string toString(const sk::util::String& x)
-    {
+    static std::string toString(const sk::util::String& x) {
         return x.inspect();
     }
 };
 
-void assertEquals(const char* expected,  const char* actual, SourceLine sourceLine, const std::string& message)
-{
-    if(expected == 0 && actual != 0) {
-        Asserter::failNotEqual("<null_ptr>", sk::util::String(actual).inspect(), sourceLine, message);
-    }
-    else if(expected != 0 && actual == 0) {
-        Asserter::failNotEqual(sk::util::String(expected).inspect(), "<null_ptr>", sourceLine, message);
-    }
-    else if(expected != 0 && actual != 0) {
-        assertEquals(sk::util::String(expected), sk::util::String(actual), sourceLine, message);
-    }
-}
-
-void assertEquals(const char* expected,  const std::string& actual, SourceLine sourceLine, const std::string& message)
-{
-    assertEquals(expected, actual.c_str(), sourceLine, message);
-}
-
-void assertEquals(const std::string& expected,  const char* actual, SourceLine sourceLine, const std::string& message)
-{
-    assertEquals(expected.c_str(), actual, sourceLine, message);
-}
-
-void assertEquals(const std::string& expected,  const std::string& actual, SourceLine sourceLine, const std::string& message)
-{
-    assertEquals(expected.c_str(), actual.c_str(), sourceLine, message);
-}
+void assertEquals(const char* expected,  const char* actual, SourceLine sourceLine, const std::string& message);
+void assertEquals(const char* expected,  const std::string& actual, SourceLine sourceLine, const std::string& message);
+void assertEquals(const std::string& expected,  const char* actual, SourceLine sourceLine, const std::string& message);
+void assertEquals(const std::string& expected,  const std::string& actual, SourceLine sourceLine, const std::string& message);
 
 CPPUNIT_NS_END
 
