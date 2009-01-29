c3bd5b4e9ffafebcc103f13091722ae9fd79f678
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-01-29 00:55:27 +0000

    Added build config for aix-53-ppc and fixed builds on HP-UX platforms.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@895 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ platform/include/hpux-legacy/sys/select.h
@@ -0,0 +1,15 @@
+/*  Copyright (c) 2008, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+*/
+
+#ifndef _PLATFORM_HPUX_SELECT_
+#define _PLATFORM_HPUX_SELECT_
+
+#include <sys/time.h>
+
+#define seteuid(euid) setreuid(-1, euid)
+
+#endif /* _PLATFORM_HPUX_SELECT_ */
