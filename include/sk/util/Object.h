af46b3a20ba74570ba422ae8d1da2a147b6c6d18
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-02-11 05:07:14 +0000

    Retabbed before changes.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@951 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- include/sk/util/Object.h
+++ include/sk/util/Object.h
@@ -20,7 +20,7 @@ namespace sk {
     class Object 
     {
       public:
-	virtual ~Object();
+        virtual ~Object();
 
         /// Returns a reference to itself.
         virtual Object& self();
@@ -30,22 +30,22 @@ namespace sk {
         /// of itself.
         Object* clone() const;
 
-	/// Returns the object's class.
-	virtual const Class getClass() const;
+        /// Returns the object's class.
+        virtual const Class getClass() const;
 
-	/// Returns a unique object id.
-	virtual long long getId() const;
+        /// Returns a unique object id.
+        virtual long long getId() const;
 
-	/// Returns a string representation of the object. By default, it
-	/// returns the object's class and the objects's id.
-	virtual const String toString() const;
+        /// Returns a string representation of the object. By default, it
+        /// returns the object's class and the objects's id.
+        virtual const String toString() const;
 
-	/// Returns a string containing human-readable representation of the
-	/// object.
-	virtual const String inspect() const;
+        /// Returns a string containing human-readable representation of the
+        /// object.
+        virtual const String inspect() const;
 
       protected:
-	Object();
+        Object();
     };
   }
 }
