59bd09a1b1ab556319360e67c50c2e59a22d5882
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-02-12 02:18:36 +0000

    Final touch: moved synchronized templates to Lockable interface from AbstractLock.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@971 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- include/sk/rt/Lockable.h
+++ include/sk/rt/Lockable.h
@@ -24,8 +24,73 @@ namespace sk {
         virtual bool tryLock() = 0;
         virtual void unlock() = 0;
         virtual void synchronize(const sk::rt::Runnable& block) = 0;
+
+        template<typename T>
+        void synchronize(T& target, void (T::*method)());
+
+        template<typename T, typename P>
+        void synchronize(T& target, void (T::*method)(P&), P& param);
+    
+        template<typename F>
+        void synchronize(const F& functor);
     };
   }
+}
+
+template<typename T>
+void 
+sk::rt::Lockable::
+synchronize(T& target, void (T::*method)())
+{
+  struct Block : public virtual sk::rt::Runnable {
+    typedef void (T::*member_function_t)();
+    Block(T& target, member_function_t method)
+      : _target(target), _method(method) {}
+
+    void run() const {
+      (_target.*_method)();
+    }
+    T& _target;
+    member_function_t _method;
+  };
+  synchronize(Block(target, method));
+}
+
+template<typename T, typename P>
+void 
+sk::rt::Lockable::
+synchronize(T& target, void (T::*method)(P&), P& param)
+{
+  struct Block : public virtual sk::rt::Runnable {
+    typedef void (T::*member_function_t)(P&);
+    Block(T& target, member_function_t method, P& param)
+      : _target(target), _method(method), _param(param) {}
+
+    void run() const {
+      (_target.*_method)(_param);
+    }
+    T& _target;
+    P& _param;
+    member_function_t _method;
+  };
+  synchronize(Block(target, method, param));
+}
+
+template<typename F>
+void 
+sk::rt::Lockable::
+synchronize(const F& functor)
+{
+  struct Block : public virtual sk::rt::Runnable {
+    Block(const F& functor)
+      : _functor(functor) {}
+
+    void run() const {
+      _functor();
+    }
+    const F& _functor;
+  };
+  synchronize(Block(functor));
 }
 
 #endif /* _SK_RT_LOCKABLE_H_ */
--- include/sk/rt/thread/AbstractLock.h
+++ include/sk/rt/thread/AbstractLock.h
@@ -36,19 +36,12 @@ namespace sk {
           void unlock();
           void synchronize(const sk::rt::Runnable& block);
 
+          using Lockable::synchronize;
+
           // sk::util::Object re-implementation.
           const sk::util::Class getClass() const;
           const sk::util::String inspect() const;
 
-          template<typename T>
-          void synchronize(T& target, void (T::*method)());
-
-          template<typename T, typename P>
-          void synchronize(T& target, void (T::*method)(P&), P& param);
-      
-          template<typename F>
-          void synchronize(const F& functor);
-      
         protected:
           AbstractLock(abstract::Mutex* mutex, bool ownership);
 
@@ -67,62 +60,6 @@ namespace sk {
       };
     }
   }
-}
-
-template<typename T>
-void 
-sk::rt::thread::AbstractLock::
-synchronize(T& target, void (T::*method)())
-{
-  struct Block : public virtual sk::rt::Runnable {
-    typedef void (T::*member_function_t)();
-    Block(T& target, member_function_t method)
-      : _target(target), _method(method) {}
-
-    void run() const {
-      (_target.*_method)();
-    }
-    T& _target;
-    member_function_t _method;
-  };
-  synchronize(Block(target, method));
-}
-
-template<typename T, typename P>
-void 
-sk::rt::thread::AbstractLock::
-synchronize(T& target, void (T::*method)(P&), P& param)
-{
-  struct Block : public virtual sk::rt::Runnable {
-    typedef void (T::*member_function_t)(P&);
-    Block(T& target, member_function_t method, P& param)
-      : _target(target), _method(method), _param(param) {}
-
-    void run() const {
-      (_target.*_method)(_param);
-    }
-    T& _target;
-    P& _param;
-    member_function_t _method;
-  };
-  synchronize(Block(target, method, param));
-}
-
-template<typename F>
-void 
-sk::rt::thread::AbstractLock::
-synchronize(const F& functor)
-{
-  struct Block : public virtual sk::rt::Runnable {
-    Block(const F& functor)
-      : _functor(functor) {}
-
-    void run() const {
-      _functor();
-    }
-    const F& _functor;
-  };
-  synchronize(Block(functor));
 }
 
 #endif /* _SK_RT_THREAD_ABSTRACTLOCK_H_ */
