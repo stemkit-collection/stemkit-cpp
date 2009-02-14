2f105aba137bd90fe4482e8e25d2202114b68d25
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-02-14 21:26:21 +0000

    Started native Windows thread implementation. Only Thread::sleep() is actually implemented for now.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@988 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/rt/thread/mswin/Implementation.cc
@@ -0,0 +1,77 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+*/
+
+#include <sk/util/Class.h>
+#include <sk/util/String.h>
+#include <sk/util/UnsupportedOperationException.h>
+
+#include "Implementation.h"
+#include <windows.h>
+
+static const sk::util::Class __class("sk::rt::thread::mswin::Implementation");
+
+sk::rt::thread::mswin::Implementation::
+Implementation()
+{
+}
+
+sk::rt::thread::mswin::Implementation::
+~Implementation()
+{
+}
+
+const sk::util::Class
+sk::rt::thread::mswin::Implementation::
+getClass() const
+{
+  return __class;
+}
+
+sk::rt::thread::abstract::Mutex* 
+sk::rt::thread::mswin::Implementation::
+makeSimpleMutex() const
+{
+  throw sk::util::UnsupportedOperationException(SK_METHOD);
+}
+
+sk::rt::thread::abstract::Mutex* 
+sk::rt::thread::mswin::Implementation::
+makeRecursiveMutex() const
+{
+  throw sk::util::UnsupportedOperationException(SK_METHOD);
+}
+
+sk::rt::thread::abstract::Thread* 
+sk::rt::thread::mswin::Implementation::
+makeThread(sk::rt::Runnable& target, sk::rt::thread::Generic& handle) const
+{
+  throw sk::util::UnsupportedOperationException(SK_METHOD);
+}
+
+sk::rt::thread::abstract::Thread* 
+sk::rt::thread::mswin::Implementation::
+wrapCurrentThread(sk::rt::thread::Generic& handle) const
+{
+  throw sk::util::UnsupportedOperationException(SK_METHOD);
+}
+
+sk::rt::thread::Generic& 
+sk::rt::thread::mswin::Implementation::
+getGeneric() const
+{
+  throw sk::util::UnsupportedOperationException(SK_METHOD);
+}
+
+void 
+sk::rt::thread::mswin::Implementation::
+sleep(uint64_t milliseconds) const
+{
+  Sleep(milliseconds);
+}
---
+++ lib/sk/rt/thread/mswin/Implementation.h
@@ -0,0 +1,47 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+*/
+
+#ifndef _SK_RT_THREAD_MSWIN_IMPLEMENTATION_H_
+#define _SK_RT_THREAD_MSWIN_IMPLEMENTATION_H_
+
+#include <sk/rt/thread/abstract/Implementation.h>
+
+namespace sk {
+  namespace rt {
+    namespace thread {
+      namespace mswin {
+        class Implementation 
+          : public virtual sk::rt::thread::abstract::Implementation
+        {
+          public:
+            Implementation();
+            virtual ~Implementation();
+        
+            // sk::rt::thread::abstract::Implementation implementation.
+            abstract::Mutex* makeSimpleMutex() const;
+            abstract::Mutex* makeRecursiveMutex() const;
+            abstract::Thread* makeThread(sk::rt::Runnable& target, sk::rt::thread::Generic& handle) const;
+            abstract::Thread* wrapCurrentThread(sk::rt::thread::Generic& handle) const;
+            sk::rt::thread::Generic& getGeneric() const;
+            void sleep(uint64_t milliseconds) const;
+
+            // sk::util::Object re-implementation.
+            const sk::util::Class getClass() const;
+        
+          private:
+            Implementation(const Implementation& other);
+            Implementation& operator = (const Implementation& other);
+        };
+      }
+    }
+  }
+}
+
+#endif /* _SK_RT_THREAD_MSWIN_IMPLEMENTATION_H_ */
