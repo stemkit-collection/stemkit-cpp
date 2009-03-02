b141afd78658261f7bf71bec39086a98af106a0f
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-03-02 19:33:27 +0000

    Updated the documentation main page.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1078 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- include/sk.h
+++ include/sk.h
@@ -23,11 +23,35 @@ namespace sk {
 /**
  *  @mainpage stemkit-cpp - All you need to start off a C++ project.
  *
- *  This library provides utility classes to use in a C++ project. Its design
- *  was inspired by standard Java class library, especialy collection
- *  framework (@java_api_link{util}) and streams (@java_api_link{io}).
+ *  This package provides various facilities helping to build C++ applications 
+ *  from scratch. Its API design was greatly inspired by standard Java class 
+ *  library, especialy collection framework (@java_api_link{util}) and streams 
+ *  (@java_api_link{io}). Its implementation, on the other hand, uses
+ *  available C++ facilities like STL and standard IO libraries, often
+ *  complicated, verbose or contrived to be used in their pure forms.
+ *
+ *  The API and implementation of stemkit-cpp is based upon a set of 
+ *  principles, following which has been proven to produce high quality, 
+ *  easy to debug code. They also help in greatly reducing possibilities 
+ *  of memory leaks. Those principles are:
+ *    -# Using pure virtual C++ protocol classes as interfaces, implemented by 
+ *       "public virtual" inheritance.
+ *    -# Avoiding multiple inheritance by all means other than interface
+ *       implementation (no more than one non-virtual inheritance).
+ *    -# All classes inherit (public virtual) from sk::util::Object, thus
+ *       having a set of guaranteed properties (object id, clone, inspect,
+ *       etc.)
+ *    -# Copy constructors and operator=() private by default, otherwise only
+ *       when proven necessary.
+ *    -# Passing pointers means passing objects' ownership. Otherwise,
+ *       object copies or references must be passed.
+ *    -# Getting a pointer is immediatelly followed by placing it in a managing
+ *       container (like sk::util::Holder, sk::util::ArrayList, std::auto_ptr, 
+ *       boost::shared_ptr, etc.).
+ *    -# Methods and method parameters (references) must be declared const by 
+ *       default, non-const only if really necessary.
  *  
- *  The functionality is split into the following namespaces:
+ *  The functionality is split into the following main namespaces:
  *
  *  <dl>
  *    <dt>sk::util
@@ -38,11 +62,26 @@ namespace sk {
  *    <dd>
  *      Provides stream classes.
  *
+ *    <dt>sk::net
+ *    <dd>
+ *      Provides network communication classes (currently almost empty).
+ *
  *    <dt>sk::rt
  *    <dd>
  *      Provides runtime support. This includes:
  *        - Scope centered logging and configuration 
- *        - Config files lookup and XML parsing
- *        - Threading and synchronization
+ *        - Configuration files lookup and XML parsing
+ *        - Thread management and synchronization
+ *        - Synchronized wrappers for sk::util containers
+ *
+ *    <dt>sk::sys
+ *    <dd>
+ *      Provides OS integration support.
+ *
+ *    <dt>sk::cppunit
+ *    <dd>
+ *      Provides useful extensions for @cppunit_link{CppUnit} unit testing 
+ *      framework. Like assertions of strings specified in any manner, with 
+ *      persentation of "ispected" content in diagnostics.
  *  </dl>
 */
