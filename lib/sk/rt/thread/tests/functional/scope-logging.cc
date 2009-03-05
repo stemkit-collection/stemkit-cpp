73d0f1436ba29245896422f5faf241b672df60c5
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-03-05 20:44:49 +0000

    Fixing a test utility to expose the probem with multiple threads trying to rotate the same log file.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1094 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- lib/sk/rt/thread/tests/functional/scope-logging.cc
+++ lib/sk/rt/thread/tests/functional/scope-logging.cc
@@ -22,12 +22,10 @@ int main(int argc, const char* argv[])
   sk::rt::Scope::controller().loadXmlConfig(
     sk::rt::config::InlineLocator("\n\
       <scope name='app'>\n\
-        <scope name='sk::rt::thread::pthread::Mutex'>\n\
-          <log destination='std::cerr' level='error' show-object='false' show-time='true' />\n\
-        </scope>\n\
-        <scope name='sk::rt::thread::pthread::Thread'>\n\
-          <log destination='std::cerr' level='error' show-object='false' show-time='true' />\n\
-        </scope>\n\
+        <log destination='file' level='notice' show-object='false' show-time='false' >\n\
+          <file location='/tmp/gfb' name='zzz' size='50M' chunks='4' ensure-chunks='true' policy='pointing' use-pipe='false' />\n\
+        </log>\n\
+        \n\
       </scope>\n\
     ")
   );
@@ -37,6 +35,44 @@ int main(int argc, const char* argv[])
   sk::rt::Thread::reset();
 }
 
+namespace {
+  struct Block : public virtual sk::rt::Runnable {
+    Block(const sk::util::String& name) 
+      : _scope(name) {}
+
+    void run() {
+      const sk::rt::thread::Generic& current = sk::rt::Thread::currentThread();
+      sk::rt::Thread::sleep(1000);
+      while(true) {
+        _scope.info("Block") << "My estimation is " << 15 << " and this is final!!! " << current.inspect() ;
+      }
+    }
+    sk::rt::Scope _scope;
+  };
+}
+
+namespace {
+  struct Starter : public virtual sk::util::Processor<sk::rt::Thread> {
+    void process(sk::rt::Thread& thread) const {
+      thread.start();
+    }
+  };
+  struct Joiner : public virtual sk::util::Processor<sk::rt::Thread> {
+    void process(sk::rt::Thread& thread) const {
+      thread.join();
+    }
+  };
+}
 void perform()
 {
+  sk::util::ArrayList<sk::rt::Thread> threads;
+
+  threads.add(new sk::rt::Thread(new Block("aaa")));
+  threads.add(new sk::rt::Thread(new Block("bbb")));
+  threads.add(new sk::rt::Thread(new Block("ccc")));
+  threads.add(new sk::rt::Thread(new Block("ddd")));
+  threads.add(new sk::rt::Thread(new Block("eee")));
+
+  threads.forEach(Starter());
+  threads.forEach(Joiner());
 }
