a1aa1ef022d25718822a7943db66624a2badd743
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-04-14 00:56:37 +0000

    Added placehoders for DaemonProcess tests.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1182 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/sys/unix/test/DaemonProcessTest.cc
@@ -0,0 +1,41 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+*/
+
+#include "DaemonProcessTest.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(sk::sys::test::DaemonProcessTest);
+
+sk::sys::test::DaemonProcessTest::
+DaemonProcessTest()
+{
+}
+
+sk::sys::test::DaemonProcessTest::
+~DaemonProcessTest()
+{
+}
+
+void
+sk::sys::test::DaemonProcessTest::
+setUp()
+{
+}
+
+void
+sk::sys::test::DaemonProcessTest::
+tearDown()
+{
+}
+
+void
+sk::sys::test::DaemonProcessTest::
+testBasics()
+{
+}
