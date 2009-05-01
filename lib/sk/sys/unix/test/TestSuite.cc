aec48854fc64f4240d90a2e386b0994ae9e610ab
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-05-01 07:47:36 +0000

    Added configurator setup.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1245 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- lib/sk/sys/unix/test/TestSuite.cc
+++ lib/sk/sys/unix/test/TestSuite.cc
@@ -8,6 +8,9 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <sk/cppunit/TestRunner.h>
 #include <sk/cppunit/SourcePath.h>
+
+#include <sk/rt/Scope.h>
+#include <sk/rt/config/InlineLocator.h>
 #include <sk/rt/Thread.h>
 
 #include <iostream>
@@ -16,6 +19,25 @@ int main(int argc, char **argv)
 {     
   CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
   sk::cppunit::TestRunner runner;
+
+  sk::rt::Scope::controller().loadXmlConfig(
+    sk::rt::config::InlineLocator("\n\
+      <scope name='app'>\n\
+        <log destination='std::cerr' eol='windows' level='info' show-object='true' show-time='true'>\n\
+          <file name='test.log' />\n\
+        </log>\n\
+        \n\
+        <scope name='thread-exception-handler'>\n\
+          <property name='abort-on-exception' value='true' />\n\
+        </scope>\n\
+        \n\
+        <scope name='sk::rt::thread::pthreads::Mutex'>\n\
+          <property name='perform-error-check' value='true' />\n\
+        </scope>\n\
+        \n\
+      </scope>\n\
+    ")
+  );
 
   if(argc == 2) {
     sk::cppunit::SourcePath::setBase(argv[1]);
