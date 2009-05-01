871481917936ba7c47e570027675b51178ddafe3
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-05-01 07:25:07 +0000

    Improved diagnostics in probem demonstration (covariant return types).
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1243 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- tests/test-covariants.cc
+++ tests/test-covariants.cc
@@ -1,10 +1,4 @@
 /*  vim: set sw=2:
- *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
- *  
- *  Distributed under the MIT Licence.
- *  This is free software. See 'LICENSE' for details.
- *  You must read and accept the license prior to use.
- *  
  *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
 */
 
@@ -14,13 +8,13 @@
 
 struct O {
   O() {
-    std::cerr << "O::O(), " << this << std::endl;
+    std::cerr << "O::O(), this=" << this << std::endl;
   }
   virtual ~O() {
-    std::cerr << "O::~O(), " << this << std::endl;
+    std::cerr << "O::~O(), this=" << this << std::endl;
   }
   virtual O* clone() const {
-    std::cerr << "O::clone(), " << this << std::endl;
+    std::cerr << "O::clone(), this=" << this << std::endl;
     throw std::string("O:clone: Unsupported");
   }
 };
@@ -29,16 +23,16 @@ struct S : public virtual O {
   virtual S* clone() const = 0;
 };
 
-struct FDIS : public virtual S {
-  FDIS() : _data(123456) {
-    std::cerr << "FDIS::FDIS(), " << this << ", data=" << _data << std::endl;
+struct FS : public virtual S {
+  FS() : _data(123456) {
+    std::cerr << "FS::FS(), this=" << this << ", data=" << _data << std::endl;
   }
-  ~FDIS() {
-    std::cerr << "FDIS::~FDIS(), " << this << ", data=" << _data << std::endl;
+  ~FS() {
+    std::cerr << "FS::~FS(), this=" << this << ", data=" << _data << std::endl;
   }
-  FDIS* clone() const {
-    std::cerr << "FDIS::clone(), " << this << ", data=" << _data << std::endl;
-    return new FDIS();
+  FS* clone() const {
+    std::cerr << "FS::clone(), this=" << this << ", data=" << _data << std::endl;
+    return new FS();
   }
 
   int _data;
@@ -46,7 +40,7 @@ struct FDIS : public virtual S {
 
 int main(int argc, const char* argv[])
 {
-  FDIS fdis;
+  FS fdis;
   S& s = fdis;
   std::auto_ptr<O> o(s.clone());
 
