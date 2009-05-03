fe90ae4a3a01b0fd17c352a7d84f22d6aaef23ba
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-05-03 02:50:45 +0000

    Adjusted compile on Mac OS 10.5 -- compensated for missing definition of external variable "environ".
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1255 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ platform/include/macos-leopard/unistd.h
@@ -0,0 +1,17 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky
+*/
+
+#ifndef _MACOS_LEOPARD_UNISTD_H_
+#define _MACOS_LEOPARD_UNISTD_H_
+
+#include <../include/unistd.h>
+extern char **environ;
+
+#endif /* _MACOS_LEOPARD_UNISTD_H_ */
