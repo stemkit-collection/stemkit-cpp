80e8e7ce0ca1f4ef2fc6d037bf9c8bb2c2d471cb
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-05-05 17:36:10 +0000

    Recreated the problem observed in MS VS 2005 - using virtually inherited temporaries in constructor initialization lists.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1284 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- tests/test-temporary-in-constructor-init-list.cc
+++ tests/test-temporary-in-constructor-init-list.cc
@@ -12,8 +12,14 @@
 #include <iomanip>
 #include <exception>
 #include <string>
+#include <memory>
 
-struct A {
+struct O {
+  O() {}
+  virtual ~O() {}
+};
+
+struct A : public virtual O {
   A(int value) : _value(value) {
     std::cerr << "A::A(int), this=" << this << ", value=" << value << std::endl;
   }
@@ -22,13 +28,15 @@ struct A {
     std::cerr << "A::A(const A&), this=" << this << ", other=" << &other << std::endl;
   }
 
-  virtual ~A() {
+  ~A() {
     std::cerr << "A::~A(), this=" << this << std::endl;
   }
 
   A duplicateA() const {
     std::cerr << "A::duplicate()" << std::endl;
-    return A(10);
+    A a(10);
+    std::cerr << "A::duplicate() - new A created, returning..." << std::endl;
+    return a;
   }
 
   int getValue() const {
@@ -45,20 +53,36 @@ struct B : public A {
   }
 
   B(const B& other) : A(other.duplicateA()) {
+  // *FIX* B(const B& other) : A(other.duplicateA().getValue()) {
     std::cerr << "B::B(const B&), this=" << this << ", other=" << &other << std::endl;
   }
-  virtual ~B() {
+  ~B() {
     std::cerr << "B::~B(), this=" << this << std::endl;
   }
 };
 
-B& makeB(B& b) {
-  std::cerr << "makeB()" << std::endl;
-  return b;
-}
+struct C : public virtual O {
+  C() : _bHolder(new B(0)) {
+    std::cerr << "C::C(), this=" << this << std::endl;
+  }
+
+  C(const C& other) : _bHolder(new B(other.getB())) {
+    std::cerr << "C::C(const C&), this=" << this << ", other=" << &other << std::endl;
+  }
+
+  ~C() {
+    std::cerr << "C::~C(), this=" << this << std::endl;
+  }
+
+  B& getB() const {
+    return *_bHolder.get();
+  };
+
+  std::auto_ptr<B> _bHolder;
+};
 
 int main(int argc, const char* argv[])
 {
-  B s = B(1);
-  B b = makeB(s);
+  C c;
+  B b = c.getB();
 }
