b8a1c13f9f896293c4ab53b5f3fe77d070eedb06
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-05-08 19:38:56 +0000

    Moved stream portal test class to platform implementation folders as they test internals that differ between implementations.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1319 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/sys/unix/test/StreamPortalTest.cc
@@ -0,0 +1,63 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+*/
+
+#include "StreamPortalTest.h"
+#include <sk/sys/StreamPortal.h>
+#include <sk/io/FileDescriptorProvider.h>
+#include <sk/io/FileDescriptor.h>
+#include <sk/util/Properties.h>
+#include <unistd.h>
+
+CPPUNIT_TEST_SUITE_REGISTRATION(sk::sys::test::StreamPortalTest);
+
+sk::sys::test::StreamPortalTest::
+StreamPortalTest()
+{
+}
+
+sk::sys::test::StreamPortalTest::
+~StreamPortalTest()
+{
+}
+
+void
+sk::sys::test::StreamPortalTest::
+setUp()
+{
+}
+
+void
+sk::sys::test::StreamPortalTest::
+tearDown()
+{
+}
+
+void
+sk::sys::test::StreamPortalTest::
+testStreams()
+{
+  CPPUNIT_ASSERT_EQUAL(45, ::dup2(1, 45));
+  CPPUNIT_ASSERT_EQUAL(55, ::dup2(1, 55));
+  CPPUNIT_ASSERT_EQUAL(65, ::dup2(1, 65));
+  {
+    sk::util::Properties registry;
+    registry.setProperty("SK_STREAMS", "45|55|65");
+
+    sk::sys::StreamPortal portal(registry);
+    CPPUNIT_ASSERT_EQUAL(3, portal.size());
+
+    CPPUNIT_ASSERT_EQUAL(45, sk::util::upcast<sk::io::FileDescriptorProvider>(portal.getStream(0)).getFileDescriptor().getFileNumber());
+    CPPUNIT_ASSERT_EQUAL(55, sk::util::upcast<sk::io::FileDescriptorProvider>(portal.getStream(1)).getFileDescriptor().getFileNumber());
+    CPPUNIT_ASSERT_EQUAL(65, sk::util::upcast<sk::io::FileDescriptorProvider>(portal.getStream(2)).getFileDescriptor().getFileNumber());
+  }
+  CPPUNIT_ASSERT_EQUAL(-1, ::close(45));
+  CPPUNIT_ASSERT_EQUAL(-1, ::close(55));
+  CPPUNIT_ASSERT_EQUAL(-1, ::close(65));
+}
