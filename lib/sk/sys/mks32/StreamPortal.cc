ba871f814d080b40020b3e501932d178698a2fc8
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-05-08 16:28:46 +0000

    Implemented fd-to-handle stream export/import under MKS32.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1315 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/sys/mks32/StreamPortal.cc
@@ -0,0 +1,21 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+*/
+
+#include <sk/sys/StreamPortal.h>
+#include <winnutc.h>
+
+void 
+sk::sys::StreamPortal::
+clear()
+{
+  for(int fd=3; fd < 1024; ++fd) {
+    ::SetHandleInformation(::_NutFdToHandle(fd), HANDLE_FLAG_INHERIT, 0);
+  }
+}
