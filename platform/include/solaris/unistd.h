9bb14e5e3238c7c5754e6f00276b285ebcc4d1aa
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-06-19 22:27:44 +0000

    Fixing compile on Solaris.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1350 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ platform/include/solaris/unistd.h
@@ -0,0 +1,26 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+*/
+
+#ifndef _PLATFORM_SOLARIS_UNISTD_H_
+#define _PLATFORM_SOLARIS_UNISTD_H_
+
+#include <../include/unistd.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+extern char** environ;
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* _PLATFORM_SOLARIS_UNISTD_H_ */
