010df3d18e877ff73f8afc66efd4e58bebec8624
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-06-21 02:33:18 +0000

    Fixing compile on AIX and HP-UX.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1352 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ platform/include/hpux/netdb.h
@@ -0,0 +1,24 @@
+/*  Copyright (c) 2006, Quest Software, http://www.quest.com
+ *  
+ *  This software is property of the mentioned copyright
+ *  holder and cannot be used or distrubuted  other than 
+ *  under a written consent agreement between individual 
+ *  parties.
+*/
+
+#ifndef _HPUX_NETDB_
+#define _HPUX_NETDB_
+
+#include <arpa/inet.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <../include/netdb.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _HPUX_NETDB_ */
---
+++ platform/include/hpux/sys/socket.h
@@ -0,0 +1,15 @@
+/*  Copyright (c) 2006, Quest Software, http://www.quest.com
+ *  
+ *  This software is property of the mentioned copyright
+ *  holder and cannot be used or distrubuted  other than 
+ *  under a written consent agreement between individual 
+ *  parties.
+*/
+
+#ifndef _HPUX_SYS_SOCKET_
+#define _HPUX_SYS_SOCKET_
+
+#include <arpa/inet.h>
+#include <../include/sys/socket.h>
+
+#endif /* _HPUX_SYS_SOCKET_ */
