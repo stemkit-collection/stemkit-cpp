d7c12119e719f5207ce43781e501739557e3dd44
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-07-14 23:16:45 +0000

    Added a probe for thread behaviour validation.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1361 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/rt/thread/tests/functional/mass-threads.cc
@@ -0,0 +1,61 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <exception>
+#include <string>
+
+#include <sk/rt/Scope.h>
+#include <sk/rt/config/InlineLocator.h>
+#include <sk/rt/Thread.h>
+
+void perform();
+
+int main(int argc, const char* argv[])
+{
+  sk::rt::Scope::controller().loadXmlConfig(
+    sk::rt::config::InlineLocator("\n\
+      <scope name='app'>\n\
+        <scope name='sk::rt::thread::pthread::Mutex'>\n\
+          <log destination='std::cerr' level='error' show-object='false' show-time='true' />\n\
+        </scope>\n\
+        <scope name='sk::rt::thread::pthread::Thread'>\n\
+          <log destination='std::cerr' level='error' show-object='false' show-time='true' />\n\
+        </scope>\n\
+      </scope>\n\
+    ")
+  );
+
+  while(true) {
+    perform();
+  }
+  sk::rt::Thread::reset();
+}
+
+namespace {
+  struct Printer : public virtual sk::rt::Runnable {
+    void run() {
+      std::cerr << sk::rt::Thread::currentThread().inspect() << std::endl;
+    }
+  };
+}
+
+void perform()
+{
+  Printer printer;
+  sk::rt::Thread thread(printer);
+
+  thread.detach();
+  thread.start();
+
+  sk::rt::Thread::sleep(1000);
+  // thread.join();
+}
