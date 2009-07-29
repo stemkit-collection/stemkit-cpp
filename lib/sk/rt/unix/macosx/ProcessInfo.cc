ada5ca0a1c6ae147e3b8cc8c7097b87e716ba4fb
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-07-29 17:06:51 +0000

    Adjusted macos implementation.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1396 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- lib/sk/rt/unix/macosx/ProcessInfo.cc
+++ lib/sk/rt/unix/macosx/ProcessInfo.cc
@@ -8,6 +8,7 @@
  *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
 */
 
+#include <sk/util/Holder.cxx>
 #include <sk/rt/ProcessInfo.h>
 #include <sk/util/IllegalStateException.h>
 
@@ -19,30 +20,37 @@
 #include <mach/task.h>
 #include <mach/mach_init.h>
 
-namespace {
-  // The actual solution is borrowed from here:
-  //   http://miknight.blogspot.com/2005/11/resident-set-size-in-mac-os-x.html
-  //
-  // Thanks to Michael Knight for posting it.
-  //
-  const struct task_basic_info sk_figure_task_info(task_t task)
+// The actual solution is borrowed from here:
+//   http://miknight.blogspot.com/2005/11/resident-set-size-in-mac-os-x.html
+//
+// Thanks to Michael Knight for posting it.
+//
+struct sk::rt::ProcessInfo::Data 
+{
+  Data(pid_t pid) : _task(MACH_PORT_NULL) {
+    if (task_for_pid(current_task(), pid, &_task) != KERN_SUCCESS) {
+      throw sk::util::IllegalStateException("Mac OS X task");
+    }
+  }
+
+  const struct task_basic_info info()
   {
     struct task_basic_info t_info;
     mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
 
-    task_info(task, TASK_BASIC_INFO, task_info_t(&t_info), &t_info_count);
+    task_info(_task, TASK_BASIC_INFO, task_info_t(&t_info), &t_info_count);
     return t_info;
   }
 
-  task_t sk_figure_task(pid_t pid)
-  {
-    task_t task = MACH_PORT_NULL;
+  private:
+    task_t _task;
+};
 
-    if (task_for_pid(current_task(), pid, &task) != KERN_SUCCESS) {
-      throw sk::util::IllegalStateException("Mac OS X task");
-    }
-    return task;
-  }
+void
+sk::rt::ProcessInfo::
+init()
+{
+  _dataHolder.set(new Data(_pid));
 }
 
 uint64_t
@@ -56,8 +64,7 @@ uint64_t
 sk::rt::ProcessInfo::
 virtualMemory() const
 {
-  const struct task_basic_info info = sk_figure_task_info(sk_figure_task(_pid));
-  return info.virtual_size;
+  return _dataHolder.get().info().virtual_size;
 }
 
 uint64_t
@@ -71,6 +78,5 @@ uint64_t
 sk::rt::ProcessInfo::
 residentMemory() const
 {
-  const struct task_basic_info info = sk_figure_task_info(sk_figure_task(_pid));
-  return info.resident_size;
+  return _dataHolder.get().info().resident_size;
 }
