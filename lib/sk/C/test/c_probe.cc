ec82e35d1681e06ccbc6e20d38f6682913ec0434
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-08-24 04:09:50 +0000

    Changed the probe C bridge implementation to exception mapping facility.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1442 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- lib/sk/C/test/c_probe.cc
+++ lib/sk/C/test/c_probe.cc
@@ -14,7 +14,12 @@ extern "C"
 const char* sk_c_test_Probe_inspect(const struct sk_c_test_ProbeHandle* handle, char* buffer, int size)
 {
   sk_c_handle::ensure_proper(handle);
-  return sk_c_handle::copy(handle->get().inspect(), buffer, size);
+  struct Action : public virtual sk::util::Mapper<sk::C::test::Probe, sk::util::String> {
+    sk::util::String map(sk::C::test::Probe& probe) const {
+      return probe.inspect();
+    }
+  };
+  return sk_c_handle::copy(handle->invoke(Action()), buffer, size);
 }
 
 extern "C"
