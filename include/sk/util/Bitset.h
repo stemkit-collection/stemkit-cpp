67794a04341cd4dacab8ab861c01a7d4c1637862
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-10-22 03:16:55 +0000

    Forgot to include the actual class files for the last commit.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1559 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ include/sk/util/Bitset.h
@@ -0,0 +1,59 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky
+*/
+
+#ifndef _SK_UTIL_BITSET_H_
+#define _SK_UTIL_BITSET_H_
+
+#include <sk/util/Object.h>
+
+namespace sk {
+  namespace util {
+    class Bitset 
+      : public virtual sk::util::Object
+    {
+      public:
+        Bitset();
+        Bitset(uint32_t min, uint32_t max);
+        virtual ~Bitset();
+
+        inline bool isOn(uint32_t index) const;
+        inline bool isOff(uint32_t index) const;
+    
+        // sk::util::Object re-implementation.
+        const sk::util::Class getClass() const;
+        const sk::util::String inspect() const;
+    
+      private:
+        Bitset(const Bitset& other);
+        Bitset& operator = (const Bitset& other);
+
+        uint32_t* _depot;
+        uint32_t _min;
+        uint32_t _max;
+
+    };
+  }
+}
+
+inline bool
+sk::util::Bitset::
+isOn(uint32_t index) const
+{
+  return (index < _min || index >= _max) ? false : _depot[index >> 5] & (1 << (index & 0x1f));
+}
+
+inline bool
+sk::util::Bitset::
+isOff(uint32_t index) const
+{
+  return !isOn(index);
+}
+
+#endif /* _SK_UTIL_BITSET_H_ */
---
+++ lib/sk/util/Bitset.cc
@@ -0,0 +1,71 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky
+*/
+
+#include <sk/util/Class.h>
+#include <sk/util/String.h>
+#include <sk/util/Bitset.h>
+
+#include <sstream>
+#include <algorithm>
+
+static const sk::util::String __className("sk::util::Bitset");
+
+static const uint32_t SK_BITMAP_BLOCKSIZE = 1024;
+
+sk::util::Bitset::
+Bitset()
+  : _depot(0), _min(0), _max(0)
+{
+}
+
+sk::util::Bitset::
+Bitset(uint32_t min, uint32_t max)
+  : _depot(0), _min((min >> 5) << 5), _max(std::max(_min, ((max >> 5) + 1) << 5))
+{
+  int size = std::max(_max - _min, SK_BITMAP_BLOCKSIZE);
+  _max = _min + size;
+
+  _depot = new uint32_t[size];
+  std::fill(_depot, _depot + size, 0);
+}
+
+sk::util::Bitset::
+~Bitset()
+{
+  if(_depot) {
+    delete [] _depot;
+  }
+}
+
+const sk::util::Class
+sk::util::Bitset::
+getClass() const
+{
+  return sk::util::Class(__className);
+}
+
+const sk::util::String
+sk::util::Bitset::
+inspect() const
+{
+  std::stringstream stream;
+  stream << _min << '<';
+
+  int size = _max - _min;
+  for(int index=0; index < size; ++index) {
+    uint32_t value = _depot[index];
+    for(int index=32; index; --index, value >>= 1) {
+      stream << '0' + (value & 1);
+    }
+  }
+  stream << '>' << _max;
+
+  return stream.str();
+}
