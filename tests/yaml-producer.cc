8b853bd5726411f478d16a31029f09f7e608052b
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-11-03 21:06:02 +0000

    Added examples of YAML generation and fixed yaml-cpp include file referencing. Fixed new tests linking on AIX.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1591 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ tests/yaml-producer.cc
@@ -0,0 +1,36 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+*/
+
+#include <iostream>
+#include <iomanip>
+
+#include <sk/other/yaml-cpp/yaml.h>
+
+int main(int argc, const char* argv[])
+{
+  YAML::Emitter out;
+  out << YAML::BeginMap;
+  out << YAML::Key << "items" << YAML::Value;
+  out << YAML::BeginSeq;
+  out << "aaa";
+  out << "bbb";
+  out << "ccc";
+
+  out << YAML::BeginMap;
+  out << YAML::Key << "v1" << YAML::Value << "aaa";
+  out << YAML::Key << "v2" << YAML::Value << "aaa";
+  out << YAML::Key << "v3" << YAML::Value << "aaa";
+  out << YAML::EndMap;
+
+  out << YAML::EndSeq;
+  out << YAML::EndMap;
+
+  std::cerr << out.c_str() << std::endl;
+}
