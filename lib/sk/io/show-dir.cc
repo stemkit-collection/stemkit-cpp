12bb7570f45e14afeeee01ed50aefcf180ad31ab
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-12-06 17:28:10 +0000

    Added a test program demostrating how to produce a directory listing with newly introduced facilities.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1605 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/io/show-dir.cc
@@ -0,0 +1,38 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky
+*/
+
+#include <iostream>
+#include <sk/util/IllegalArgumentException.h>
+#include <sk/util/Pathname.h>
+#include <sk/io/FileInfo.h>
+#include <sk/io/Dir.h>
+
+namespace {
+  struct Printer : public virtual sk::util::Processor<const sk::io::FileInfo> {
+    void process(const sk::io::FileInfo& info) const {
+      std::cout << info << std::endl;
+    }
+  };
+}
+
+int main(int argc, const char* argv[])
+{
+  sk::util::Pathname program(argv[0]);
+  try {
+    if(argc != 2) {
+      throw sk::util::IllegalArgumentException("USAGE: " + program.basename() + " <directory>");
+    }
+    sk::io::Dir dir(argv[1]);
+    dir.forEachEntry(Printer());
+  }
+  catch(const std::exception& exception) {
+    std::cerr << exception.what() << std::endl;
+  }
+}
