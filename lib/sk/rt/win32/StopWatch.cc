665e96c9a6cf32a64498de6558da215af35afa73
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-12-23 18:05:33 +0000

    Implemented windows specifics for obtaining current time value.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1686 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- lib/sk/rt/win32/StopWatch.cc
+++ lib/sk/rt/win32/StopWatch.cc
@@ -18,6 +18,8 @@
 #include <windows.h>
 
 struct sk::rt::StopWatch::Data : public virtual sk::util::Object {
+  struct timeval start;
+  struct timeval stop;
 };
 
 void
@@ -28,6 +30,33 @@ init()
 }
 
 namespace {
+  int gettimeofday(struct timeval* tp, void* n) {
+    LARGE_INTEGER ticksPerSecond;
+    LARGE_INTEGER tick;   // A point in time
+    LARGE_INTEGER time;   // For converting tick into real time
+
+    // get the high resolution counter's accuracy
+    // if this function failed, return 0
+    if(QueryPerformanceFrequency(&ticksPerSecond) == 0) {
+      return -1;
+    }
+
+    // what time is it?
+    if(QueryPerformanceCounter(&tick) == 0) {
+      return -1;
+    }
+
+    // convert the tick number into the number of seconds
+    // since the system was started...
+    time.QuadPart = tick.QuadPart / ticksPerSecond.QuadPart;
+
+    tp->tv_sec = time.QuadPart; /* seconds */
+    tp->tv_usec = (tick.QuadPart - (time.QuadPart * ticksPerSecond.QuadPart)) * 
+                  1000000.0 / ticksPerSecond.QuadPart;
+
+    return 0;
+  }
+  
   void obtain_current_time(struct timeval& timeinfo) {
     if(gettimeofday(&timeinfo, 0) != 0) {
       throw sk::rt::SystemException("gettimeofday");
