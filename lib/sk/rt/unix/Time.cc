40d48d179ea1a09ae9da6db9c7e37709d0dcc24f
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-12-23 18:40:15 +0000

    Added ability to obtain current time and put it in struct timeval. On Unix, it is performed by function gettimeofday().
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1687 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- lib/sk/rt/unix/Time.cc
+++ lib/sk/rt/unix/Time.cc
@@ -11,14 +11,25 @@
 #include <sk/util/Class.h>
 #include <sk/util/String.h>
 #include <sk/util/UnsupportedOperationException.h>
+#include <sk/rt/SystemException.h>
 
 #include <sk/rt/Time.h>
 #include <time.h>
+#include <sys/time.h>
 
 const struct tm&
 sk::rt::Time::
-figure_localtime(struct tm& tm_buffer) const
+makeLocaltime(struct tm& tm_buffer) const
 {
-  localtime_r(&_seconds, &tm_buffer);
+  ::localtime_r(&_seconds, &tm_buffer);
   return tm_buffer;
+}
+
+void 
+sk::rt::Time::
+obtainCurrentTime(struct timeval& timeinfo) 
+{
+  if(::gettimeofday(&timeinfo, 0) != 0) {
+    throw sk::rt::SystemException("gettimeofday");
+  }
 }
