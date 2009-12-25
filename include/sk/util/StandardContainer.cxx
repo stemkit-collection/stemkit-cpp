baf4d41e11769528d9e6e1d36cc5ffdfff149b93
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-12-25 04:12:20 +0000

    Started filling up methods of STL containers based master class.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1706 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- include/sk/util/StandardContainer.cxx
+++ include/sk/util/StandardContainer.cxx
@@ -12,6 +12,10 @@
 #define _SK_UTIL_STANDARDCONTAINER_CXX_
 
 #include <sk/util/StandardContainer.hxx>
+#include <sk/util/StreamLiner.h>
+#include <sk/util/NoSuchElementException.h>
+#include <sk/util/selector/EqualPointer.hxx>
+#include <algorithm>
 
 template<typename T, typename Policy, typename Type>
 sk::util::StandardContainer<T, Policy, Type>::
@@ -23,13 +27,154 @@ template<typename T, typename Policy, typename Type>
 sk::util::StandardContainer<T, Policy, Type>::
 ~StandardContainer()
 {
+  sk::util::Exception::guard(StreamLiner(std::cerr), *this, &StandardContainer<T, Policy, Type>::clear, __FUNCTION__);
 }
+
+template<typename T, typename Policy, typename Type>
+bool 
+sk::util::StandardContainer<T, Policy, Type>::
+isEmpty() const
+{
+  return _container.empty() == true;
+}
+
+template<typename T, typename Policy, typename Type>
+int 
+sk::util::StandardContainer<T, Policy, Type>::
+size() const
+{
+  return _container.size();
+}
+
+template<typename T, typename Policy, typename Type>
+struct sk::util::StandardContainer<T, Policy, Type>::Cleaner : std::unary_function<typename Type::item_t, void> {
+  void operator()(typename Type::item_t& item) {
+    Policy::clearSlot(item);
+  }
+};
 
 template<typename T, typename Policy, typename Type>
 void
 sk::util::StandardContainer<T, Policy, Type>::
-process(T& object)
+clear()
 {
+  std::for_each(_container.begin(), _container.end(), Cleaner());
+  return _container.clear();
+}
+
+template<typename T, typename Policy, typename Type>
+struct sk::util::StandardContainer<T, Policy, Type>::SelectingFunctor : std::unary_function<typename Type::item_t, bool> {
+  SelectingFunctor(const sk::util::Selector<T>& selector)
+    : _selector(selector) {}
+
+  bool operator()(const typename Type::item_t& item) {
+    return _selector.assess(Policy::getObject(item));
+  }
+  const sk::util::Selector<T>& _selector;
+};
+
+template<typename T, typename Policy, typename Type>
+const T& 
+sk::util::StandardContainer<T, Policy, Type>::
+get(const Selector<T>& selector) const
+{
+  typename container_t::const_iterator iterator = std::find_if(_container.begin(), _container.end(), SelectingFunctor(selector));
+  if(iterator == _container.end()) {
+    throw sk::util::NoSuchElementException(SK_METHOD);
+  }
+  return Policy::getObject(*iterator);
+}
+
+template<typename T, typename Policy, typename Type>
+T& 
+sk::util::StandardContainer<T, Policy, Type>::
+getMutable(const Selector<T>& selector)
+{
+  typename container_t::const_iterator iterator = std::find_if(_container.begin(), _container.end(), SelectingFunctor(selector));
+  if(iterator == _container.end()) {
+    throw sk::util::NoSuchElementException(SK_METHOD);
+  }
+  return Policy::getMutableObject(*iterator);
+}
+
+template<typename T, typename Policy, typename Type>
+bool 
+sk::util::StandardContainer<T, Policy, Type>::
+find(sk::util::Holder<T>& holder, const Selector<T>& selector) const
+{
+  typename container_t::const_iterator iterator = std::find_if(_container.begin(), _container.end(), SelectingFunctor(selector));
+  if(iterator == _container.end()) {
+    return false;
+  }
+  holder.set(Policy::getObject(*iterator));
+  return true;
+}
+
+template<typename T, typename Policy, typename Type>
+bool 
+sk::util::StandardContainer<T, Policy, Type>::
+findMutable(sk::util::Holder<T>& holder, const Selector<T>& selector)
+{
+  typename container_t::const_iterator iterator = std::find_if(_container.begin(), _container.end(), SelectingFunctor(selector));
+  if(iterator == _container.end()) {
+    return false;
+  }
+  holder.set(Policy::getMutableObject(*iterator));
+  return true;
+}
+
+template<typename T, typename Policy, typename Type>
+struct sk::util::StandardContainer<T, Policy, Type>::ConstProcessingFunctor : std::unary_function<typename Type::item_t, void> {
+  ConstProcessingFunctor(const sk::util::Processor<const T>& processor)
+    : _processor(processor) {}
+
+  void operator()(const typename Type::item_t& item) const {
+    _processor.process(Policy::getObject(item));
+  }
+  const sk::util::Processor<const T>& _processor;
+};
+
+template<typename T, typename Policy, typename Type>
+void 
+sk::util::StandardContainer<T, Policy, Type>::
+forEach(const Processor<const T>& processor) const
+{
+  std::for_each(_container.begin(), _container.end(), ConstProcessingFunctor(processor));
+}
+
+template<typename T, typename Policy, typename Type>
+struct sk::util::StandardContainer<T, Policy, Type>::ProcessingFunctor : std::unary_function<typename Type::item_t, void> {
+  ProcessingFunctor(const sk::util::Processor<T>& processor)
+    : _processor(processor) {}
+
+  void operator()(const typename Type::item_t& item) const {
+    _processor.process(Policy::getMutableObject(item));
+  }
+  const sk::util::Processor<T>& _processor;
+};
+
+template<typename T, typename Policy, typename Type>
+void 
+sk::util::StandardContainer<T, Policy, Type>::
+forEach(const Processor<T>& processor)
+{
+  std::for_each(_container.begin(), _container.end(), ProcessingFunctor(processor));
+}
+
+template<typename T, typename Policy, typename Type>
+bool 
+sk::util::StandardContainer<T, Policy, Type>::
+contains(const T& object) const
+{
+  return contains(sk::util::selector::EqualPointer<T>(object));
+}
+
+template<typename T, typename Policy, typename Type>
+bool 
+sk::util::StandardContainer<T, Policy, Type>::
+contains(const Selector<T>& selector) const
+{
+  return std::find_if(_container.begin(), _container.end(), SelectingFunctor(selector)) != _container.end();
 }
 
 #endif /* _SK_UTIL_STANDARDCONTAINER_CXX_ */
--- include/sk/util/StandardContainer.hxx
+++ include/sk/util/StandardContainer.hxx
@@ -11,16 +11,90 @@
 #ifndef _SK_UTIL_STANDARDCONTAINER_HXX_
 #define _SK_UTIL_STANDARDCONTAINER_HXX_
 
+#include <sk/util/Collection.h>
+#include <sk/util/List.h>
+#include <sk/util/String.h>
+
 namespace sk {
   namespace util {
     template<typename T, typename Policy, typename Type>
     class StandardContainer 
+      : public virtual sk::util::Collection<T>,
+        public virtual sk::util::List<T>
     {
       public:
         StandardContainer();
         ~StandardContainer();
-    
-        void process(T& object);
+
+        // sk::util::Collection implementation.
+        const T& get(const Selector<T>& selector) const;
+        T& getMutable(const Selector<T>& selector);
+
+        bool find(sk::util::Holder<T>& holder, const Selector<T>& selector) const;
+        bool findMutable(sk::util::Holder<T>& holder, const Selector<T>& selector);
+
+        void forEach(const Processor<const T>& processor) const;
+        void forEach(const Processor<T>& processor);
+
+        bool isEmpty() const;
+        int size() const;
+
+        bool contains(const T& object) const;
+        bool contains(const Selector<T>& selector) const;
+        bool containsAll(const Collection<T>& other) const;
+        bool containsAll(const Collection<T>& other, const sk::util::BinaryAssessor<T>& assessor) const;
+
+        bool add(const T& object);
+        bool add(T& object);
+        bool add(T* object);
+
+        void clear();
+        bool remove(const T& object);
+        bool remove(const Selector<T>& selector);
+
+        T* cutoff(const T& object);
+        T* cutoff(const Selector<T>& selector);
+        T* release(const T& object);
+        T* release(const Selector<T>& selector);
+
+        bool removeAll(const Collection<T>& other);
+        bool removeAll(const Collection<T>& other, const sk::util::BinaryAssessor<T>& assessor);
+        bool removeAll(const Selector<T>& selector);
+
+        bool retainAll(const Collection<T>& other);
+        bool retainAll(const Collection<T>& other, const sk::util::BinaryAssessor<T>& assessor);
+        bool retainAll(const Selector<T>& selector);
+        
+        // sk::util::List implementation.
+        void add(int index, const T& object);
+        void add(int index, T& object);
+        void add(int index, T* object);
+
+        const T& get(int index) const;
+        T& getMutable(int index);
+
+        int indexOf(const T& object) const;
+        int indexOf(const Selector<T>& selector) const;
+        int lastIndexOf(const T& object) const;
+        int lastIndexOf(const Selector<T>& selector) const;
+
+        void remove(int index);
+        T* cutoff(int index);
+        T* release(int index);
+
+        void set(int index, const T& object);
+        void set(int index, T& object);
+        void set(int index, T* object);
+
+        void sort();
+        void sort(const sk::util::BinaryAssessor<T>& assessor);
+        void shuffle();
+        void reverse();
+        
+        // sk::util::Object implementation.
+        const sk::util::Class getClass() const;
+        const String inspect() const;
+        const String toString() const;
     
       private:
         StandardContainer(const StandardContainer<T, Policy, Type>& other);
@@ -28,6 +102,11 @@ namespace sk {
 
         typedef typename Type::item_t item_t;
         typedef typename Type::container_t container_t;
+
+        struct Cleaner;
+        struct SelectingFunctor;
+        struct ConstProcessingFunctor;
+        struct ProcessingFunctor;
 
         container_t _container;
     };
