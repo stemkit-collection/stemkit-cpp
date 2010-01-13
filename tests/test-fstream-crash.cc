cfdbfe41336210f774cb260d42d128fbf7fd4c7e
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2010-01-13 20:59:50 +0000

    Added a test program demonstrating SIGSEGV in ifstream new/delete (AIX, -O2).
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1764 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ tests/test-fstream-crash.cc
@@ -0,0 +1,39 @@
+/*  vi: sw=2:
+ *  Copyright (c) 2006, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+*/
+
+#include <iostream>
+ 
+#include <sk/util/Pathname.h>
+#include <fstream>
+#include <memory>
+
+class MyStream 
+{
+  public:
+    MyStream(const char* name)
+      : _stream(name) {}
+
+  private:
+    std::ifstream _stream;
+};
+
+int main(int argc, char **argv)
+{     
+  try {
+    // MyStream* stream = new MyStream("abc"); // NO CRASH
+    std::ifstream* stream = new std::ifstream("abc"); // CRASH
+    std::cerr << ">>> OPENED: " << stream << std::endl;
+    delete stream;
+    std::cerr << ">>> DELETED" << std::endl;
+  }
+  catch(const std::exception& exception) {
+    std::cerr << "E: " << exception.what() << std::endl;
+  }
+  std::cerr << ">>> CLEANED" << std::endl;
+  return 0;
+}
