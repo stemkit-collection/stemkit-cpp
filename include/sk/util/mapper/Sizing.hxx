eb9d3eef0c079af96b9d67fab9aab8b8e8d059fc
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2010-04-18 07:50:15 +0000

    Added a mapper that obtains an object's size().
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@1995 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ include/sk/util/mapper/Sizing.hxx
@@ -0,0 +1,30 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+*/
+
+#ifndef _SK_UTIL_MAPPER_SIZING_HXX_
+#define _SK_UTIL_MAPPER_SIZING_HXX_
+
+#include <sk/util/Mapper.h>
+
+namespace sk {
+  namespace util {
+    namespace mapper {
+      template<typename T>
+      class Sizing : public virtual sk::util::Mapper<const T, const int> {
+        public:
+          const int map(const T& object) const {
+            return object.size();
+          }
+      };
+    }
+  }
+}
+
+#endif /* _SK_UTIL_MAPPER_SIZING_HXX_ */
