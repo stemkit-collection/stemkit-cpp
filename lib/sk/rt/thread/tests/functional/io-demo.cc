0ba29dbfc9c4c82a1ecfd9c82d5725d10632ac68
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2010-04-25 07:12:45 +0000

    Some more demos.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2004 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/rt/thread/tests/functional/io-demo.cc
@@ -0,0 +1,29 @@
+#include <sk/io/File.h>
+#include <sk/io/IOException.h>
+#include <sk/io/DataOutputStream.h>
+#include <sk/net/DataOutputStream.h>
+#include <sk/io/BufferedOutputStream.h>
+#include <iostream>
+
+void writeData(sk::io::DataOutput& data) {
+  data.writeInt(12);
+  data.writeLong(1234);
+}
+
+int main(int argc, const char* const argv[]) {
+  try { 
+    sk::io::File f1("host_ordered_data", "w");
+    sk::io::BufferedOutputStream host_stream(f1.outputStream());
+    sk::io::DataOutputStream host_data(host_stream);
+
+    sk::io::File f2("network_ordered_data", "w");
+    sk::io::BufferedOutputStream network_stream(f2.outputStream());
+    sk::net::DataOutputStream network_data(network_stream);
+
+    writeData(host_data);
+    writeData(network_data);
+  }
+  catch(const sk::io::IOException& exception) {
+    std::cerr << "IO failed: " << exception.getMessage() << std::endl;
+  }
+}
---
+++ lib/sk/rt/thread/tests/functional/util-demo.cc
@@ -0,0 +1,27 @@
+#include <sk/util/ArrayList.cxx>
+#include <sk/util/LinkedList.cxx>
+#include <sk/util/String.h>
+
+void print_list(const sk::util::List<sk::util::String>& list)
+{
+  std::cerr << list.inspect() << std::endl;
+}
+
+int main(int argc, const char* const argv[])
+{
+  sk::util::String s("  abcdefg");
+
+  s.inspect(); // "abcdefg"
+  s.trim().startsWith("abc"); // true
+  s.toLowerCase().endsWith("efg"); // true
+
+  sk::util::ArrayList<sk::util::String> array_list;
+  array_list.add(s);
+  array_list.add(new sk::util::String("zzz"));
+  
+  sk::util::LinkedList<sk::util::String> linked_list;
+  linked_list.add(s);
+
+  print_list(array_list);
+  print_list(linked_list);
+}
