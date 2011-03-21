cbd0b11b880557490674297e0f37872e72206831
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-03-21 05:44:48 +0000

    Introduced a class to encapsulate pthread condition operations.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2175 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/rt/thread/pthreads/Condition.cc
@@ -0,0 +1,70 @@
+/*  vim: sw=2:
+ *  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky
+*/
+
+#include <sk/util/Class.h>
+#include <sk/util/String.h>
+#include <sk/rt/TimeoutException.h>
+
+#include "Condition.h"
+#include "Exception.h"
+
+#include <errno.h>
+
+static const sk::util::String __className("sk::rt::thread::pthreads::Condition");
+
+sk::rt::thread::pthreads::Condition::
+Condition(pthreads::Mutex& mutex)
+  : _scope(__className), _mutexHandle(mutex.getHandle())
+{
+    SK_PTHREAD_RAISE_UNLESS_SUCCESS(pthread_cond_init(&_conditionHandle, 0));
+}
+
+sk::rt::thread::pthreads::Condition::
+~Condition()
+{
+  sk::util::Exception::guard(_scope.warning(), *this, &Condition::cleanup, __FUNCTION__);
+}
+
+void
+sk::rt::thread::pthreads::Condition::
+cleanup()
+{
+  SK_PTHREAD_RAISE_UNLESS_SUCCESS(pthread_cond_destroy(&_conditionHandle));
+}
+
+const sk::util::Class
+sk::rt::thread::pthreads::Condition::
+getClass() const
+{
+  return sk::util::Class(__className);
+}
+
+pthread_cond_t&
+sk::rt::thread::pthreads::Condition::
+getHandle()
+{
+  return _conditionHandle;
+}
+
+void
+sk::rt::thread::pthreads::Condition::
+waitUntil(const struct timespec& moment)
+{
+  if(SK_PTHREAD_RAISE_UNLESS_SUCCESS_OR(ETIMEDOUT, pthread_cond_timedwait(&_conditionHandle, &_mutexHandle, &moment)) == false) {
+    throw sk::rt::TimeoutException(SK_METHOD);
+  }
+}
+
+void
+sk::rt::thread::pthreads::Condition::
+wait()
+{
+  SK_PTHREAD_RAISE_UNLESS_SUCCESS(pthread_cond_wait(&_conditionHandle, &_mutexHandle));
+}
---
+++ lib/sk/rt/thread/pthreads/Condition.h
@@ -0,0 +1,53 @@
+/*  vim: sw=2:
+ *  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky
+*/
+
+#ifndef _SK_RT_THREAD_PTHREADS_CONDITION_H_
+#define _SK_RT_THREAD_PTHREADS_CONDITION_H_
+
+#include <sk/util/Object.h>
+#include "Mutex.h"
+
+#include <pthread.h>
+
+namespace sk {
+  namespace rt {
+    namespace thread {
+      namespace pthreads {
+        class Condition 
+          : public virtual sk::util::Object
+        {
+          public:
+            Condition(pthreads::Mutex& mutex);
+            virtual ~Condition();
+
+            pthread_cond_t& getHandle();
+
+            void wait();
+            void waitUntil(const struct timespec& moment);
+
+            // sk::util::Object re-implementation.
+            const sk::util::Class getClass() const;
+        
+          private:
+            Condition(const Condition& other);
+            Condition& operator = (const Condition& other);
+
+            void cleanup();
+
+            pthread_mutex_t& _mutexHandle;
+            pthread_cond_t _conditionHandle;
+            sk::rt::Scope _scope;
+        };
+      }
+    }
+  }
+}
+
+#endif /* _SK_RT_THREAD_PTHREADS_CONDITION_H_ */
