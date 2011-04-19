926422c1e1cd283765f09a24f48b8f3e1f2d7e1c
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-04-19 05:17:10 +0000

    Added a class for performing persistent actions.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2234 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ include/sk/rt/Actions.h
@@ -0,0 +1,119 @@
+/*  vim: sw=2:
+ *  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky
+*/
+
+#ifndef _SK_RT_ACTIONS_H_
+#define _SK_RT_ACTIONS_H_
+
+#include <sk/util/Object.h>
+#include <sk/rt/Runnable.h>
+#include <sk/util/ArrayList.hxx>
+#include <sk/util/Exception.h>
+#include <sk/rt/Scope.h>
+
+namespace sk {
+  namespace rt {
+    class Actions 
+      : public virtual sk::util::Object
+    {
+      public:
+        Actions(const sk::rt::Scope& scope);
+        virtual ~Actions();
+
+        template<typename T, typename TMF> 
+        void add(const sk::util::String& label, T& target, TMF method);
+    
+        template<typename T, typename TMF, typename P> 
+        void add(const sk::util::String& label, T& target, TMF method, P& param);
+    
+        template<typename T, typename TMF, typename P> 
+        void add(const sk::util::String& label, T& target, TMF method, const P& param);
+
+        void perform();
+        void performReverse();
+        void performReverseThrow(const sk::util::Exception& exception);
+    
+        // sk::util::Object re-implementation.
+        const sk::util::Class getClass() const;
+    
+        struct Item : public sk::util::String {
+          Item(const sk::util::String& label);
+          virtual void invoke() const = 0;
+        };
+
+      private:
+        Actions(const Actions& other);
+        Actions& operator = (const Actions& other);
+
+        void addItem(Item* actionItem);
+        void finalize();
+
+        const sk::rt::Scope& _scope;
+        sk::util::ArrayList<Item> _items;
+        sk::util::ArrayList<sk::util::Exception> _exceptions;
+
+        template<typename T, typename TMF> 
+        struct MemberFunctionInvocator;
+
+        template<typename T, typename TMF, typename P> 
+        struct MemberFunctionWithParamInvocator;
+    };
+  }
+}
+
+template<typename T, typename TMF>
+struct sk::rt::Actions::MemberFunctionInvocator : public virtual sk::rt::Actions::Item {
+  MemberFunctionInvocator(const sk::util::String& label, T& target, TMF method)
+    : Item(label), _target(target), _method(method) {}
+
+  void invoke() const {
+    (_target.*_method)();
+  }
+  T& _target;
+  TMF _method;
+};
+
+template<typename T, typename TMF>
+void
+sk::rt::Actions::
+add(const sk::util::String& label, T& target, TMF method)
+{
+  return addItem(new MemberFunctionInvocator<T, TMF>(label, target, method));
+}
+
+template<typename T, typename TMF, typename P>
+struct sk::rt::Actions::MemberFunctionWithParamInvocator : public virtual sk::rt::Actions::Item {
+  MemberFunctionWithParamInvocator(const sk::util::String& label, T& target, TMF method, P& param)
+    : Item(label), _target(target), _method(method), _param(param) {}
+
+  void invoke() const {
+    (_target.*_method)(_param);
+  }
+  T& _target;
+  TMF _method;
+  P& _param;
+};
+
+template<typename T, typename TMF, typename P>
+void
+sk::rt::Actions::
+add(const sk::util::String& label, T& target, TMF method, P& param)
+{
+  return addItem(new MemberFunctionWithParamInvocator<T, TMF, P>(label, target, method, param));
+}
+
+template<typename T, typename TMF, typename P>
+void
+sk::rt::Actions::
+add(const sk::util::String& label, T& target, TMF method, const P& param)
+{
+  return addItem(new MemberFunctionWithParamInvocator<T, TMF, const P>(label, target, method, param));
+}
+
+#endif /* _SK_RT_ACTIONS_H_ */
---
+++ lib/sk/rt/Actions.cc
@@ -0,0 +1,97 @@
+/*  vim: sw=2:
+ *  Copyright (c) 2011, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky
+*/
+
+#include <sk/util/Class.h>
+#include <sk/util/String.h>
+#include <sk/util/ArrayList.cxx>
+
+#include <sk/rt/Actions.h>
+
+static const sk::util::String __className("sk::rt::Actions");
+
+sk::rt::Actions::Item::
+Item(const sk::util::String& label)
+  : sk::util::String(label)
+{
+}
+
+sk::rt::Actions::
+Actions(const sk::rt::Scope& scope)
+  : _scope(scope)
+{
+}
+
+sk::rt::Actions::
+~Actions()
+{
+}
+
+const sk::util::Class
+sk::rt::Actions::
+getClass() const
+{
+  return sk::util::Class(__className);
+}
+
+void 
+sk::rt::Actions::
+addItem(Item* item)
+{
+  _items.add(item);
+}
+
+namespace {
+  struct GuardingExecutor : public virtual sk::util::Processor<const sk::rt::Actions::Item> {
+    GuardingExecutor(sk::util::List<sk::util::Exception>& exceptions)
+      : _exceptions(exceptions) {}
+
+    void process(const sk::rt::Actions::Item& item) const {
+      try {
+        item.invoke();
+      }
+      catch(const sk::util::Exception& exception) {
+        _exceptions.add(exception);
+      }
+    }
+    sk::util::List<sk::util::Exception>& _exceptions;
+  };
+}
+
+void 
+sk::rt::Actions::
+perform()
+{
+  _items.forEach(GuardingExecutor(_exceptions));
+  finalize();
+}
+
+void 
+sk::rt::Actions::
+performReverse()
+{
+  _items.forEach(GuardingExecutor(_exceptions));
+  finalize();
+}
+
+void 
+sk::rt::Actions::
+performReverseThrow(const sk::util::Exception& exception)
+{
+  _exceptions.add(exception);
+  _items.forEach(GuardingExecutor(_exceptions));
+  finalize();
+}
+
+void
+sk::rt::Actions::
+finalize()
+{
+  _items.clear();
+}
