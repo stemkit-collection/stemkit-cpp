dc84243864fc050c7c5d12cd7e886505a0eced96
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-04-29 02:46:27 +0000

    Implemented passive side of sk::net::Socket.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2326 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- include/sk/net/Socket.h
+++ include/sk/net/Socket.h
@@ -12,11 +12,16 @@
 #define _SK_NET_SOCKET_H_
 
 #include <sk/util/Object.h>
+#include <sk/util/Holder.hxx>
+#include <sk/io/InputStream.h>
+#include <sk/io/OutputStream.h>
+
+#include <sk/net/InetSocketAddress.h>
 
 namespace sk {
   namespace net {
     class DirectedSocket;
-    class InetSocketAddress;
+    class InetAddress;
     class ServerSocket;
 
     class Socket 
@@ -24,6 +29,17 @@ namespace sk {
     {
       public:
         virtual ~Socket();
+
+        uint16_t getLocalPort() const;
+        const sk::net::InetAddress& getLocalAddress() const;
+
+        uint16_t getRemotePort() const;
+        const sk::net::InetAddress& getRemoteAddress() const;
+
+        sk::io::InputStream& inputStream() const;
+        sk::io::OutputStream& outputStream() const;
+
+        void close();
     
         // sk::util::Object re-implementation.
         const sk::util::Class getClass() const;
@@ -31,6 +47,9 @@ namespace sk {
       private:
         Socket(sk::net::DirectedSocket* directedSocket, const sk::net::InetSocketAddress& localSocketAddress);
         Socket& operator = (const Socket& other);
+
+        sk::util::Holder<sk::net::DirectedSocket>::Sharing _directedSocketHolder;
+        sk::util::Holder<sk::net::InetSocketAddress>::Copying _localEndpointHolder;
 
         friend class sk::net::ServerSocket;
     };
--- lib/sk/net/Socket.cc
+++ lib/sk/net/Socket.cc
@@ -10,14 +10,18 @@
 
 #include <sk/util/Class.h>
 #include <sk/util/String.h>
+#include <sk/util/Holder.cxx>
 
 #include <sk/net/Socket.h>
+#include <sk/net/InetAddress.h>
+
 #include "DirectedSocket.h"
 
 static const sk::util::String __className("sk::net::Socket");
 
 sk::net::Socket::
 Socket(sk::net::DirectedSocket* directedSocket, const sk::net::InetSocketAddress& localSocketAddress)
+  : _directedSocketHolder(directedSocket), _localEndpointHolder(localSocketAddress)
 {
 }
 
@@ -31,4 +35,54 @@ sk::net::Socket::
 getClass() const
 {
   return sk::util::Class(__className);
+}
+
+uint16_t 
+sk::net::Socket::
+getLocalPort() const
+{
+  return _localEndpointHolder.get().getPort();
+}
+
+const sk::net::InetAddress& 
+sk::net::Socket::
+getLocalAddress() const
+{
+  return _localEndpointHolder.get().getAddress();
+}
+
+uint16_t 
+sk::net::Socket::
+getRemotePort() const
+{
+  return _directedSocketHolder.get().getPort();
+}
+
+const sk::net::InetAddress& 
+sk::net::Socket::
+getRemoteAddress() const
+{
+  return _directedSocketHolder.get().getAddress();
+}
+
+sk::io::InputStream& 
+sk::net::Socket::
+inputStream() const
+{
+  return _directedSocketHolder.get().inputStream();
+}
+
+sk::io::OutputStream& 
+sk::net::Socket::
+outputStream() const
+{
+  return _directedSocketHolder.get().outputStream();
+}
+
+void 
+sk::net::Socket::
+close()
+{
+  _directedSocketHolder.clear();
+  _localEndpointHolder.clear();
 }
