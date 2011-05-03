da83fcd3c75ab27c69cdc7856605e51c7c020ed7
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-05-03 05:37:15 +0000

    Improved format of runtime system exception diagnostics.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2372 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- lib/sk/rt/unix/SystemException.cc
+++ lib/sk/rt/unix/SystemException.cc
@@ -20,28 +20,28 @@ static const char* __className("sk::rt::SystemException");
 namespace {
   const sk::util::String get_errno_message(int code) {
     const char* message = strerror(code);
-    if(message != 0) {
-      return message;
+    if(message == 0) {
+      message = "Unknown system error";
     }
-    return "Error " + sk::util::String::valueOf(code);
+    return sk::util::String(message) + " (" + sk::util::String::valueOf(code) + ")";
   }
 }
 
 sk::rt::SystemException::
 SystemException(const sk::util::String& message)
-  : sk::util::Exception(sk::util::Strings("Runtime") << message << sk::util::String::valueOf(errno) << get_errno_message(errno)), _code(errno)
+  : sk::util::Exception(sk::util::Strings("Runtime") << message << get_errno_message(errno)), _code(errno)
 {
 }
 
 sk::rt::SystemException::
 SystemException(const sk::util::String& message, uint32_t code)
-  : sk::util::Exception(sk::util::Strings("Runtime") << message << sk::util::String::valueOf(code) << get_errno_message(code)), _code(code)
+  : sk::util::Exception(sk::util::Strings("Runtime") << message << get_errno_message(code)), _code(code)
 {
 }
 
 sk::rt::SystemException::
 SystemException(const sk::util::String& message, const sk::util::Strings& details)
-  : sk::util::Exception(sk::util::Strings("Runtime") << message << sk::util::String::valueOf(errno) << get_errno_message(errno) << details), _code(errno)
+  : sk::util::Exception(sk::util::Strings("Runtime") << message << get_errno_message(errno) << details), _code(errno)
 {
 }
 
