dced6d05ab2b2f0735f436665d8b645c4c1c17cf
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-05-04 22:16:19 +0000

    Implementing proper console on Windows.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2391 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/io/win32/Console.cc
@@ -0,0 +1,73 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky (gennady.bystritsky@quest.com)
+*/
+
+#include <sk/util/Class.h>
+#include <sk/util/String.h>
+#include <sk/util/Holder.cxx>
+
+#include <sk/io/Console.h>
+#include <sk/io/FileDescriptorInputStream.h>
+#include <sk/io/FileDescriptorOutputStream.h>
+
+#include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+
+static const sk::util::String __className("sk::io::Console");
+
+struct sk::io::Console::Data : public virtual sk::util::Object {
+  sk::util::Holder<sk::io::InputStream> inputStreamHolder;
+  sk::util::Holder<sk::io::OutputStream> outputStreamHolder;
+};
+
+sk::io::Console::
+Console()
+  : _dataHolder(new Data()), _data(_dataHolder.getMutable())
+{
+  AllocConsole();
+
+  _data.inputStreamHolder.set(new sk::io::FileDescriptorInputStream(_open_osfhandle(DWORD(GetStdHandle(STD_INPUT_HANDLE)), _O_TEXT)));
+  _data.outputStreamHolder.set(new sk::io::FileDescriptorOutputStream(_open_osfhandle(DWORD(GetStdHandle(STD_OUTPUT_HANDLE)), _O_TEXT)));
+}
+
+sk::io::Console::
+~Console()
+{
+  close();
+}
+
+const sk::util::Class
+sk::io::Console::
+getClass() const
+{
+  return sk::util::Class(__className);
+}
+
+void
+sk::io::Console::
+close()
+{
+  _data.inputStreamHolder.clear();
+  _data.outputStreamHolder.clear();
+}
+
+sk::io::InputStream&
+sk::io::Console::
+inputStream() const
+{
+  return _data.inputStreamHolder.getMutable();
+}
+
+sk::io::OutputStream&
+sk::io::Console::
+outputStream() const
+{
+  return _data.outputStreamHolder.getMutable();
+}
