5318bdc27d25677d64b9d2174c3e8ac84487d572
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-08-04 21:28:24 +0000

    Introduced "unsupported" implementation for IPv6. Currently is referenced from HP-UX 11.11 build, where IPv6 support is broken -- it is defined only when _XOPEN_SOURCE_EXTENDED is not defined, which breaks much more code.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2506 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ lib/sk/net/ip6/unsupported/InetAddress.cc
@@ -0,0 +1,28 @@
+/*  vim: set sw=2:
+ *  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+ *  
+ *  Distributed under the MIT Licence.
+ *  This is free software. See 'LICENSE' for details.
+ *  You must read and accept the license prior to use.
+ *  
+ *  Author: Gennady Bystritsky
+*/
+
+#include <sk/util/Class.h>
+#include <sk/util/Strings.h>
+#include <sk/util/UnsupportedOperationException.h>
+
+#include <sk/net/ip6/InetAddress.h>
+
+namespace {
+  const sk::util::bytes& figure_address_components(const sk::util::String& name, const struct addrinfo& info) {
+    throw sk::util::UnsupportedOperationException("IPv6 resolution", name);
+  }
+}
+
+sk::net::ip6::InetAddress::
+InetAddress(const sk::util::String& name, const struct addrinfo& info)
+  : sk::net::InetAddress(figure_address_components(name, info))
+{
+}
+
