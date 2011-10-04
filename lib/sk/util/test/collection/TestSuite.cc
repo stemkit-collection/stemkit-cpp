ed6b530442efe95652544577dd069a444f771b79
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-10-04 05:14:08 +0000

    svn merge -c2530 %r/trunk . Backporting compile fix for AIX.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2532 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- lib/sk/util/test/collection/TestSuite.cc
+++ lib/sk/util/test/collection/TestSuite.cc
@@ -8,6 +8,8 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <sk/cppunit/TestRunner.h>
 #include <sk/cppunit/SourcePath.h>
+#include <sk/util/test/Probe.cxx>
+#include <sk/util/String.h>
 
 #include <iostream>
  
@@ -22,3 +24,9 @@ int main(int argc, char **argv)
   runner.addTest(registry.makeTest());
   return !runner.run();
 }
+
+// Forcing template instantiation for the types used in other
+// test classes.
+
+template class sk::util::test::Probe<sk::util::String>;
+
