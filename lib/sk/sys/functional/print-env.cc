bf250deec8e78fde37b3cfe029d4c45932fefe7d
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-11-06 07:57:46 +0000

    Added ability to specify on the command line what environment variables should be printed out.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2589 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- lib/sk/sys/functional/print-env.cc
+++ lib/sk/sys/functional/print-env.cc
@@ -10,14 +10,26 @@
 
 #include <iostream>
 #include <sk/rt/Environment.h>
+#include <sk/util/Strings.h>
 
 int main(int argc, const char* const argv[])
 {
-  struct Printer : public virtual sk::util::BinaryProcessor<const sk::util::String, const sk::util::String> {
-      void process(const sk::util::String& key, const sk::util::String& value) const {
-          std::cout << key.inspect() << "=" << value.inspect() << std::endl;
-      }
-  };
-  sk::rt::Environment().forEach(Printer());
-  return 0;
+    sk::util::Strings args;
+    for(int index=1; index < argc; ++index) {
+        args << sk::util::String(argv[index]).toLowerCase();
+    }
+
+    struct Printer : public virtual sk::util::BinaryProcessor<const sk::util::String, const sk::util::String> {
+        Printer(const sk::util::Strings& args)
+            : _args(args) {}
+
+        void process(const sk::util::String& key, const sk::util::String& value) const {
+            if(_args.isEmpty() == true || _args.contains(key.toLowerCase()) == true) {
+                std::cout << key.inspect() << "=" << value.inspect() << std::endl;
+            }
+        }
+        const sk::util::Strings& _args;
+    };
+    sk::rt::Environment().forEach(Printer(args));
+    return 0;
 }
